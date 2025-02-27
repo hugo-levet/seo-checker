1a5f387d05559789b73c2475e98089a9473c42cf
Hugo Levet <hugolevet.pro@gmail.com>
2025-02-27 17:44:00 +0100

    Add styles for Twitter Card summary and update HTML for preview

--- twitter_card_summary.php
+++ twitter_card_summary.php
@@ -13,13 +13,25 @@
     <meta name="twitter:title" content="Twitter Card Summary">
     <meta name="twitter:description" content="This is a test for Twitter Card Summary.">
     <meta name="twitter:image" content="<?= BASE_URL ?>/images/card_summary.png">
-    <meta name="twitter:image:alt" content="Hugo Levet Logo">
+    <meta name="twitter:image:alt" content="Twitter Card Summary">
+    <link rel="stylesheet" href="/styles/twitter.min.css">
 </head>
 
 <body>
     <p>This page is a test for Twitter Card Summary.</p>
-    <p>Check this tweet to see the result: <a href="https://twitter.com/hugolevet_pro" target="_blank">Twitter Card
+    <p>Check this tweet to see the result in Twitter: <a href="https://twitter.com/hugolevet_pro" target="_blank">Twitter Card
             Summary</a></p>
+    <p>Here is a preview of the Twitter Card Summary:</p>
+    <div class="preview-container">
+        <a class="twitter-card" href="" target="_blank">
+            <img src="<?= BASE_URL ?>/images/card_summary.png" alt="Twitter Card Summary">
+            <div class="text">
+                <div class="link">seo-checker.link</div>
+                <div class="title">Twitter Card Summary</div>
+                <div class="description">This is a test for Twitter Card Summary.</div>
+            </div>
+        </a>
+    </div>
 </body>
 
 </html>
